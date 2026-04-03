6bab24745fc247a269914a2ba402651a65a5f833
zsy-sk <1929714967@qq.com>
2026-04-03 09:59:49 +0800

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,9 +1,16 @@
-const CACHE_NAME = 'fieldog-pwa-v1'
-const SHELL = ['./', './index.html', './manifest.webmanifest', './favicon.svg', './icons.svg']
+const CACHE_NAME = 'fieldog-pwa-v3'
+const OFFLINE_URL = '/index.html'
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(SHELL))
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/manifest.webmanifest',
+        '/favicon.svg'
+      ])
+    })
   )
   self.skipWaiting()
 })
@@ -11,35 +18,20 @@ self.addEventListener('install', event => {
 self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) return caches.delete(key)
+        })
+      )
     )
   )
   self.clients.claim()
 })
 
 self.addEventListener('fetch', event => {
-  if (event.request.method !== 'GET') return
-  const url = new URL(event.request.url)
   if (event.request.mode === 'navigate') {
     event.respondWith(
-      fetch(event.request).then(response => {
-        const copy = response.clone()
-        caches.open(CACHE_NAME).then(cache => cache.put('./index.html', copy))
-        return response
-      }).catch(() => caches.match('./index.html'))
-    )
-    return
-  }
-  if (url.origin === self.location.origin) {
-    event.respondWith(
-      caches.match(event.request).then(cached => {
-        if (cached) return cached
-        return fetch(event.request).then(response => {
-          const copy = response.clone()
-          caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy))
-          return response
-        })
-      })
+      fetch(event.request).catch(() => caches.match(OFFLINE_URL))
     )
   }
 })
